11d241ca293f314bc5cb35651cabb3d80634e680
princekushwaha <57499371+princekushwaha@users.noreply.github.com>
2020-03-13 23:06:57 +0530

    Update create_new.php

--- create_new.php
+++ create_new.php
@@ -19,6 +19,21 @@
       <input  class="signbtn" type="button" value="  Create  ">
     </form>
  </div>
+  <script>
+  if(window.innerWidth < 478)
+   {
+     var div=document.getElementById('divi');
+     div.style.width="100%";
+     div.style.height="100%";
+     var form=document.forms[0];
+     form.style.transform="translate(-50%,-50%)";
+     form.elements[0].style.width="80%";
+     form.elements[1].style.width="80%";
+     form.elements[2].style.width="80%";
+     form.elements[3].style.width="80%";
+     form.elements[4].style.width="80%";
+   }
+  </script>
   <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
