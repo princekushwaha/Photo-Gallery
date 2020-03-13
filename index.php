ade890be3431ab016038704dcf05d88028ff43a0
princekushwaha <57499371+princekushwaha@users.noreply.github.com>
2020-03-13 22:52:45 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,40 @@
+
+ <html class="loginhtml">
+  <head>
+    <title>Registration Page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="css/style.css" rel="stylesheet" type="text/css"></link>
+  </head>
+  <body class="loginBody">
+
+  <div class="div1" id="div2">
+    <form class="form">
+      <img src="img/logo.png" class="logo"><label class="logo_name">Photo Gallery</label></img><br>
+      <label class="signin">Sign In</label><br>
+      <input type="email" placeholder="Email" class="inp"><br>
+      <input type="password" placeholder="Password" class="inp"><br><br>
+      No Account? <a href="create_new.php">Create One!</a><br>
+      <input  class="signbtn" type="button" value="Sign In">
+    </form>
+ </div>
+ <script>
+ </script>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script>
+   if(window.innerWidth < 478)
+    {
+
+      var div=document.getElementById('div2');
+      div.style.width="100%";
+      div.style.height="100%";
+
+      var form=document.forms[0];
+      form.style.transform="translate(-50%,-50%)";
+    }
+   </script>
+  </body>
+
+ </html>
